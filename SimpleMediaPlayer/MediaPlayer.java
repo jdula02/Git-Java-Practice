5b61f47a3a3864ee979c06e0bc448fa2fe6d1124
jdula02 <dulajb@appstate.edu>
2024-08-23 11:08:17 -0400

    Java Docs added for Video and Audio Player

--- SimpleMediaPlayer/MediaPlayer.java
+++ SimpleMediaPlayer/MediaPlayer.java
@@ -2,6 +2,8 @@ package SimpleMediaPlayer;
 
 public interface MediaPlayer 
 {
+ // Simple play method for either media
+ // This is the super class method.
  public void play();
 } 
 
--- SimpleMediaPlayer/VideoPlayer.java
+++ SimpleMediaPlayer/VideoPlayer.java
@@ -1,6 +1,7 @@
 package SimpleMediaPlayer;
 
 public class VideoPlayer implements MediaPlayer {
+    //Sub class play method for VideoPlayer
     public void play() {
         System.out.println("Playing a video file...");
     }
